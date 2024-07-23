ae43c3cc84c5cdf458fd7e01055e00be764efd0d
gobanoff <a.kliosovas@yahoo.com>
2024-07-23 11:20:02 +0300

    api commit

--- api/businesses.js
+++ api/businesses.js
@@ -1,12 +1,23 @@
 import { MongoClient } from 'mongodb';
 
 const uri = process.env.MONGODB_URI;
-let client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+let client;
+let clientPromise;
+
+if (!clientPromise) {
+  client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  clientPromise = client.connect();
+}
 
 export default async function handler(req, res) {
-  if (!client.isConnected()) await client.connect();
-  const db = client.db('test');
-  const collection = db.collection('businesses'); 
-  const documents = await collection.find({}).toArray();
-  res.status(200).json(documents);
+  try {
+    const client = await clientPromise;
+    const db = client.db('test');
+    const collection = db.collection('businesses'); 
+    const documents = await collection.find({}).toArray();
+    res.status(200).json(documents);
+  } catch (error) {
+    console.error('Error connecting to database:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 }
