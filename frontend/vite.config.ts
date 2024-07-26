370b37e5b9b39a92925ce88dc5270d9d2c0bc7d9
gobanoff <a.kliosovas@yahoo.com>
2024-07-26 09:41:52 +0300

    api

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -18,7 +18,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://home-service-project-jade.vercel.app",
+        //"http://localhost:3000",
         changeOrigin: true,
      
       },
