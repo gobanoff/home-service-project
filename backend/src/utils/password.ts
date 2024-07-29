05e310c06e3f405fc26db5ffe70082caaf11754b
gobanoff <a.kliosovas@yahoo.com>
2024-07-30 01:11:34 +0300

    a

--- backend/src/utils/password.ts
+++ backend/src/utils/password.ts
@@ -4,9 +4,7 @@ import { Types } from "mongoose";
 const expiresIn = "90d";
 
 export const generateToken = (payload: Types.ObjectId) => {
-  if (!process.env.JWT_SECRET) {
-    throw new Error("JWT_SECRET is not defined in the environment variables");
-  }
+  
       const token = jwt.sign({payload}, process.env.JWT_SECRET!, { expiresIn });
   return token;
 };
