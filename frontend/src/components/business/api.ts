63242dbe2b0ef86b41142a716338cf11016148cd
gobanoff <a.kliosovas@yahoo.com>
2024-07-29 16:26:08 +0300

    api commit

--- frontend/src/components/business/api.ts
+++ frontend/src/components/business/api.ts
@@ -2,6 +2,6 @@ import axiosInstance from "@/config/axios";
 import { Business } from "./types";
 
 export const fetchBusinesses = async (): Promise<Business[]> => {
-  const response = await axiosInstance.get("/businesses");
+  const response = await axiosInstance.get("/api/businesses");
   return await response.data;
 };
--- frontend/src/components/category/api.ts
+++ frontend/src/components/category/api.ts
@@ -2,6 +2,6 @@ import axiosInstance from "@/config/axios";
 import { Category } from "./types";
 
 export const fetchCategories = async (): Promise<Category[]> => {
-  const response = await axiosInstance.get("/categories");
+  const response = await axiosInstance.get("/api/categories");
   return await response.data;
 };
