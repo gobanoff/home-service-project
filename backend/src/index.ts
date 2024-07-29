4b05cddaa4d5b42bbaa8c18aed2e00c338362b16
gobanoff <a.kliosovas@yahoo.com>
2024-07-29 17:22:23 +0300

    api commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,11 +15,9 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY || '');
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(
-  cors({
-    origin: 'https://home-service-project-jade.vercel.app',
-  }),
-);
+//app.use(
+  //cors({
+    //origin: 'https://home-service-project-jade.vercel.app', }),);
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.static(path.join(__dirname, '../../frontend/dist')));
