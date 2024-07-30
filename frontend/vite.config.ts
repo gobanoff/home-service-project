cb45ab9b33a5ab71aa5738a25e70eaea08d0208e
gobanoff <a.kliosovas@yahoo.com>
2024-07-31 01:00:07 +0300

    api

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,18 +14,16 @@ export default defineConfig({
       },
     },
   },
-  //server: {
-   //// proxy: {
-    //  "/api": {
-       // target: "http://localhost:3000",
-       // changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, ""), },  },  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
   build: {
     outDir: "dist",
-   
-    chunkSizeWarningLimit: 1000, 
   },
 });
-
-
- 
