5f4a845ccdcb275edfdfcd2bb5b4283789686d09
gobanoff <a.kliosovas@yahoo.com>
2024-08-05 15:31:49 +0300

    api  commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import path from 'path';
+//import path from 'path';
 import authRoutes from './routes/authRoutes';
 import categoryRoutes from './routes/categoryRoutes';
 import businessRoutes from './routes/businessRoutes';
@@ -13,20 +13,18 @@ dotenv.config();
 sgMail.setApiKey(process.env.SENDGRID_API_KEY || '');
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(cors());
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+//app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
 app.use('/auth', authRoutes);
 app.use('/categories', categoryRoutes);
 app.use('/businesses', businessRoutes);
 app.use('/bookings', bookingRoutes);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/dist', 'index.html'));
-});
+//app.get('*', (req, res) => {
+// res.sendFile(path.join(__dirname, '../../frontend/dist', 'index.html'));});
 
 app.post('/email', async (req, res) => {
   const { to, subject, text, html } = req.body;
@@ -55,5 +53,3 @@ connectToDb()
   .catch((err) => {
     console.error('Failed to connect to the database', err);
   });
-
-
